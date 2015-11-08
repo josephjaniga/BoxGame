e8dfc7a6747984d3f97d97f8b5ff162796ec7cbc
erikbelusic <erikbelusic@Eriks-Air.home>
2015-11-08 18:53:33 -0500

    assign local myId based on socket connection id

--- client/js/app.js
+++ client/js/app.js
@@ -97,10 +97,7 @@ var app = new Vue({
 
         this.socket = io.connect(socketHref);
         this.socket.on('connect', function () {
-            console.log(this.socket.id);
-            self.socket.on('assignId', function (data) {
-                self.myId = data;
-            });
+            self.myId = self.socket.id;
 
             self.socket.on('update', function (data) {
                 self.boxes = data.boxes;
