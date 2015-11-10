e14af7ecf4bc6c84552c88bc38d63e03e7aeeb73
Joe Janiga <josephjaniga@gmail.com>
2015-11-09 19:57:52 -0500

    rewrote server implementation based on submodule POC 'to-market'

--- server/app.js
+++ server/app.js
@@ -1,119 +1,30 @@
 var express = require('express'),
     app = express(),
     server = require('http').Server(app),
+    pig = require('to-market')(),
     io = require('socket.io')(server);
 
 server.listen(process.env.PORT || 1337);
 
 app.use('/', express.static('client'));
 
-var lastFrameTime = new Date().getTime(),
-    thisFrameTime = new Date().getTime();
-
-var boxes = { };
-
 io.on('connection', function (socket) {
 
     console.log(socket.id + " - connection started");
-    playerConnect(socket);
+    pig.playerConnect(socket);
 
     socket.on('disconnect', function () {
         console.log("disconnected - " + socket.id);
-        playerDisconnect(socket);
+        pig.playerDisconnect(socket);
     });
 
-    socket.on('keyStateChange', function(keyStateObject){
-        var playerString = getNameOfPlayerById(socket.id);
-        if ( playerString !== null ){
-            boxes[playerString].keyIsPressed = keyStateObject;
-        }
+    socket.on('keyStateChange', (keyStateObject)=>{
+        pig.keyStateChange(keyStateObject, socket);
     });
 
 });
 
-var Update = function(){
-    // frameStart
-    console.log(new Date().getTime());
-    handleInput();
-    Update();
-};
-
-Update();
-
 // the Update Loop
 setInterval(function(){
-    io.emit('update', {boxes: boxes});
+    io.emit('update', {boxes: pig.entities});
 }, 10);
-
-/**
- * DEFINITIONS
- */
-
-function playerConnect(socket){
-    readyPlayer(socket.id);
-}
-
-function playerDisconnect(socket){
-    deletePlayer(socket.id);
-}
-
-function getNameOfPlayerById( id ){
-    var playerName = null;
-    for ( var property in boxes ){
-        if ( boxes[property].id === id ){
-            playerName = property;
-            break;
-        }
-    }
-    return playerName;
-}
-
-function handleInput( ){
-
-    //for(var x=0; x<9999999; x++){
-    //    Math.random();
-    //}
-
-    thisFrameTime = new Date().getTime();
-
-    var deltaTime = (thisFrameTime - lastFrameTime) / 1000;
-
-    var speed = 50 * deltaTime;
-
-    for ( var property in boxes ){
-        if ( boxes[property].keyIsPressed.up ){
-            boxes[property].y += speed;
-        }
-        if ( boxes[property].keyIsPressed.down ){
-            boxes[property].y -= speed;
-        }
-        if ( boxes[property].keyIsPressed.right ){
-            boxes[property].x += speed;
-        }
-        if ( boxes[property].keyIsPressed.left ){
-            boxes[property].x -= speed;
-        }
-    }
-
-    lastFrameTime = thisFrameTime;
-}
-
-function readyPlayer(id){
-    var playerName = id;
-    boxes[playerName] = {
-        id: id,
-        color:"#"+((1<<24)*Math.random()|0).toString(16),
-        y:0,
-        x:0,
-        keyIsPressed: {
-            up: false,
-            down: false,
-            left: false,
-            right: false,
-        }
-    };
-}
-
-function deletePlayer(id){
-    delete boxes[id];
-}
