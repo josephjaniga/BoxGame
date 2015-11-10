670f49b5a6a886271fdb813c750f71787db1f1f9
josephjaniga <josephjaniga@gmail.com>
2015-11-09 23:48:34 -0500

    updated the implementation

--- server/app.js
+++ server/app.js
@@ -1,33 +1,22 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').Server(app),
-    pig = require('to-market'),
-    io = require('socket.io')(server);
+var pig = require('to-market');
 
-// set the server to listen on a port
-server.listen(process.env.PORT || 1337);
-
-// statically serve the client directory at root
-app.use('/', express.static('client'));
-
-
-io.on('connection', function (socket) {
-
-    console.log(socket.id + " - connection started");
-    pig.playerConnect(socket);
-
-    socket.on('disconnect', function () {
-        console.log("disconnected - " + socket.id);
-        pig.playerDisconnect(socket);
-    });
-
-    socket.on('keyStateChange', (keyStateObject)=>{
-        pig.keyStateChange(keyStateObject, socket);
-    });
-
+pig({
+    connect: (id) => {
+        pig.entities[id] = {
+            id: id,
+            color:"#"+((1<<24)*Math.random()|0).toString(16),
+            y:0,
+            x:0,
+            keyIsPressed: {
+                up: false,
+                down: false,
+                left: false,
+                right: false
+            }
+        };
+    },
+    disconnect: (id)=>{
+        delete pig.entities[id];
+    }
 });
 
-// Broadcast Object State
-setInterval(function(){
-    io.emit('update', {entities: pig.getEntityLiterals()});
-}, 10);
