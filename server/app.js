dbbaacc6b358312aba6e19787078ea8681eb424a
erikbelusic <erikbelusic@outlook.com>
2015-11-21 13:34:49 -0500

    move basic map into separate file and pass it into game

--- server/app.js
+++ server/app.js
@@ -16,18 +16,18 @@ gameServerInstance.init({
             entity = new Entity({name: id, game: gameServerInstance}),
             transformComponent = entity.GetComponent("Transform"),
             colliderComponent = new Collider({}),
-            rigidbodyComponent = new Rigidbody({transform:transformComponent, collider:colliderComponent});
+            rigidbodyComponent = new Rigidbody({transform: transformComponent, collider: colliderComponent});
 
         // add the components
         entity.addComponents([rendererComponent, characterMotionComponent, colliderComponent, rigidbodyComponent]);
         // add it to the server
         gameServerInstance.addEntities([entity]);
     },
-    disconnect: (id)=>{
+    disconnect: (id)=> {
         // on disconnect remove the entity added for the client
         var entity = gameServerInstance.getEntityByName(id),
             index = gameServerInstance.entities.indexOf(entity);
-        if ( index > -1 ){
+        if (index > -1) {
             gameServerInstance.entities.splice(index, 1);
         }
         delete gameServerInstance.clients[id];
@@ -41,50 +41,11 @@ gameServerInstance.init({
  */
 
 // PLATFORMS
-(()=>{
+(()=> {
 
-    var platformColor = gameServerInstance.getRandomColor();
-
-    var platformOne = new PhysicsEntity({
-            name: "PLATFORM ONE",
-            color: platformColor,
-            useGravity: false,
-            isKinematic: true,
-            rendererType: "Rectangle",
-            x:-100,
-            y:300,
-            w:300,
-            h:50,
-            game: gameServerInstance
-        }),
-        platformTwo = new PhysicsEntity({
-            name: "PLATFORM TWO",
-            color: platformColor,
-            useGravity: false,
-            isKinematic: true,
-            rendererType: "Rectangle",
-            x:250,
-            y:250,
-            w:300,
-            h:50,
-            game: gameServerInstance
-        }),
-        platformThree = new PhysicsEntity({
-            name: "PLATFORM THREE",
-            color: platformColor,
-            useGravity: false,
-            isKinematic: true,
-            rendererType: "Rectangle",
-            x:600,
-            y:200,
-            w:300,
-            h:50,
-            game: gameServerInstance
-        });
-
-    gameServerInstance.addEntities([platformOne, platformTwo, platformThree]);
+    gameServerInstance.addEntities(require('./map1')(gameServerInstance));
 
 })();
 
-Physics.Raycast({point:{x:300,y:20}, direction:{x:0,y:1}, game:gameServerInstance, distance: 300});
-Physics.Raycast({point:{x:0,y:0}, direction:{x:1,y:-1}, game:gameServerInstance, distance: 1000});
+//Physics.Raycast({point: {x: 300, y: 20}, direction: {x: 0, y: 1}, game: gameServerInstance, distance: 300});
+//Physics.Raycast({point: {x: 0, y: 0}, direction: {x: 1, y: -1}, game: gameServerInstance, distance: 1000});
